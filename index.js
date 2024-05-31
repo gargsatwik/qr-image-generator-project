bdea279cb16a01980b2474a63437274ba215769b
Satwik Garg <satwikgarg@Satwiks-MacBook-Air.local>
2024-05-31 13:16:59 +0530

    minor improvements

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import inquirer from "inquirer"
-import * as qr from "qr-image"
-import {createWriteStream, writeFile} from "fs"
+import qr from "qr-image"
+import fs from "fs"
 
 const question = {
     "type": "input",
@@ -11,12 +11,12 @@ const question = {
 inquirer.prompt(question)
   .then(answers => {
     const url = answers["URL"];
-    writeFile("url.txt", `${url}`, (err)=>{
+    fs.writeFile("url.txt", `${url}`, (err)=>{
         if (err) throw err;
         console.log("success")
     })
     var qr_img = qr.image(`${url}`, {type: "png"})
-    qr_img.pipe(createWriteStream(`${url}.png`))
+    qr_img.pipe(fs.createWriteStream(`${url}.png`))
   })
   .catch(error => {
     console.error(error);
